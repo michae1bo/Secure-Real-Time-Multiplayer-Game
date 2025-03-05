758821008d7cc794b66625668aee7e89e3706c2c
michae1bo <159921466+michae1bo@users.noreply.github.com>
2025-03-05 21:10:46 +0000

    feat: configure helmetjs

--- server.js
+++ server.js
@@ -4,11 +4,23 @@ const bodyParser = require('body-parser');
 const expect = require('chai');
 const socket = require('socket.io');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner.js');
 
 const app = express();
+app.use(helmet({
+  noSniff: true,
+  xssFilter: true,
+  noCache: true
+}))
+
+
+app.use((req, res, next) => {
+  res.setHeader('X-Powered-By', 'PHP 7.4.3');
+  next();
+})
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/assets', express.static(process.cwd() + '/assets'));
