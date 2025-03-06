3f50953e643f5439b22af35b7d7e03a0db19f27c
michae1bo <159921466+michae1bo@users.noreply.github.com>
2025-03-06 21:56:31 +0000

    feat: implement Player and Collectible class

--- public/Player.mjs
+++ public/Player.mjs
@@ -1,18 +1,47 @@
 class Player {
   constructor({x, y, score, id}) {
+    this.id = id;
+    this.score = score;
+    this.x = x;
+    this.y = y;
 
   }
 
   movePlayer(dir, speed) {
-
+    switch(dir) {
+      case 'up':
+        this.y += speed;
+        break;
+      case 'down':
+        this.y -= speed;
+        break;
+      case 'right':
+        this.x += speed;
+        break;
+      case 'left':
+        this.x -= speed;
+        break;
+    }
   }
 
   collision(item) {
-
+    if (this.x === item.x && this.y === item.y) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   calculateRank(arr) {
-
+    let rank = 1;
+    const totalPlayers = arr.length;
+    for (let i = 0; i < totalPlayers; i++) {
+      if (arr[i].score > this.score) {
+        console.log('hi')
+        rank++;
+      }
+    }
+    return `Rank: ${rank}/${totalPlayers}`;
   }
 }
 
